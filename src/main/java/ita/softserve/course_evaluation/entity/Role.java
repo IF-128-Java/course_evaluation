e374af34713eee747dc6481a7491abf5d1d72bb1
Vova <vovapro111@gmail.com>
2021-07-12 13:58:27 +0300

    created Role

---
+++ src/main/java/ita/softserve/course_evaluation/entity/Role.java
@@ -0,0 +1,51 @@
+package ita.softserve.course_evaluation.entity;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_name", length = 20, unique = true)
+    private ERole roleName;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "role_permission",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    private Set<Permission> permissions = new HashSet<>();
+
+    public Role() {
+    }
+
+    public Role(ERole roleName, Set<Permission> permissions) {
+        this.roleName = roleName;
+        this.permissions = permissions;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public ERole getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(ERole roleName) {
+        this.roleName = roleName;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+}
