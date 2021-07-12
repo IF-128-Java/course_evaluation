85b62fa766d0576fe78c19fed71c60aba037a8ea
Vova <vovapro111@gmail.com>
2021-07-12 14:03:58 +0300

    created NullReferenceEntityException

---
+++ src/main/java/ita/softserve/course_evaluation/exception/NullEntityReferenceException.java
@@ -0,0 +1,9 @@
+package ita.softserve.course_evaluation.exception;
+
+public class NullEntityReferenceException extends RuntimeException {
+    public NullEntityReferenceException() {    }
+
+    public NullEntityReferenceException(String message) {
+        super(message);
+    }
+}
