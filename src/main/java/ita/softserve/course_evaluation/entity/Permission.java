ef37a3c6094d6c17eab220e5993e97e747d43150
Vova <vovapro111@gmail.com>
2021-07-12 13:57:53 +0300

    created Permission

---
+++ src/main/java/ita/softserve/course_evaluation/entity/Permission.java
@@ -0,0 +1,62 @@
+package ita.softserve.course_evaluation.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
+
+@Entity
+@Table(name = "permission")
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "The 'permission_name' cannot be empty")
+    @Column(name = "permission_name", nullable = false)
+    private String permissionName;
+
+    @Column(name = "description")
+    private String description;
+
+    public Permission() {
+    }
+
+    public Permission(String permissionName, String description) {
+        this.permissionName = permissionName;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPermissionName() {
+        return permissionName;
+    }
+
+    public void setPermissionName(String permissionName) {
+        this.permissionName = permissionName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Permission that = (Permission) o;
+        return Objects.equals(id, that.id) && Objects.equals(permissionName, that.permissionName) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, permissionName, description);
+    }
+}
+
