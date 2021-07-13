d62c80b6365b3c7c55f2bfedbc93341796dc3270
Vova <vovapro111@gmail.com>
2021-07-13 12:10:31 +0300

    added findPermissionsByRole_Id

--- src/main/java/ita/softserve/course_evaluation/repository/PermissionRepository.java
+++ src/main/java/ita/softserve/course_evaluation/repository/PermissionRepository.java
@@ -3,10 +3,12 @@ package ita.softserve.course_evaluation.repository;
 import ita.softserve.course_evaluation.entity.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
 
     Optional<Permission> findByPermissionName(String name);
 
+    List<Permission> findPermissionsByRole_Id(long id);
 }
