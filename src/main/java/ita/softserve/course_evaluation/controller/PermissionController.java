84762d3073ec20949744d938351a9b4524d7aed0
Vova <vovapro111@gmail.com>
2021-07-16 19:16:04 +0300

    remove import wildcard and rename endpoints (add s to the end)

--- src/main/java/ita/softserve/course_evaluation/controller/PermissionController.java
+++ src/main/java/ita/softserve/course_evaluation/controller/PermissionController.java
@@ -5,15 +5,21 @@ import ita.softserve.course_evaluation.entity.Permission;
 import ita.softserve.course_evaluation.service.PermissionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/permission")
+@RequestMapping("api/v1/permissions")
 public class PermissionController {
-
-private final PermissionService permissionService;
+    private final PermissionService permissionService;
 
     public PermissionController(PermissionService permissionService) {
         this.permissionService = permissionService;
@@ -30,11 +36,13 @@ private final PermissionService permissionService;
         return ResponseEntity.status(HttpStatus.OK)
                 .body(permissionService.readById(id));
     }
+
     @GetMapping("role/{id}")
     public ResponseEntity<List<PermissionDto>> getPermissionByRoleId(@PathVariable long id) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(permissionService.getByRoleId(id));
     }
+
     @PostMapping
     public ResponseEntity<Permission> addPermission(@RequestBody PermissionDto dto) {
         return ResponseEntity.status(HttpStatus.OK).body(permissionService.create(dto));
--- src/main/java/ita/softserve/course_evaluation/controller/RoleController.java
+++ src/main/java/ita/softserve/course_evaluation/controller/RoleController.java
@@ -5,28 +5,38 @@ import ita.softserve.course_evaluation.entity.Role;
 import ita.softserve.course_evaluation.service.RoleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/role")
+@RequestMapping("api/v1/roles")
 public class RoleController {
     private final RoleService roleService;
 
     public RoleController(RoleService roleService) {
         this.roleService = roleService;
     }
+
     @GetMapping
     public ResponseEntity<List<RoleDto>> getPermissions() {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(roleService.getAll());
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<RoleDto> getPermissionById(@PathVariable long id) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(roleService.readById(id));
     }
+
     @PostMapping
     public ResponseEntity<Role> addPermission(@RequestBody RoleDto dto) {
         return ResponseEntity.status(HttpStatus.OK).body(roleService.create(dto));
@@ -39,7 +49,6 @@ public class RoleController {
 
     @PutMapping("/{id}")
     public ResponseEntity<RoleDto> editPermission(@RequestBody RoleDto dto, @PathVariable long id) {
-        dto.setId(id);
         return ResponseEntity.status(HttpStatus.OK).body(roleService.update(dto));
     }
 
