102b2cf20b0179ef677db838d61192a65fbd945a
Vova <vovapro111@gmail.com>
2021-07-16 19:19:05 +0300

    add check for null values

--- src/main/java/ita/softserve/course_evaluation/dto/PermissionDtoMapper.java
+++ src/main/java/ita/softserve/course_evaluation/dto/PermissionDtoMapper.java
@@ -8,22 +8,25 @@ import java.util.stream.Collectors;
 
 public class PermissionDtoMapper {
 
-    public static Permission fromDto(PermissionDto dto){
-        Permission permission = new Permission(dto.getPermissionName(), dto.getDescription());
-        permission.setId(dto.getId());
-        Role role= new Role();
-        role.setId(dto.getRole_id());
-        permission.setRole(role);
+    public static Permission fromDto(PermissionDto dto) {
+        Permission permission = null;
+        if (dto != null) {
+            Role role = new Role();
+            role.setId(dto.getRole_id());
+            permission = new Permission(dto.getId(), dto.getPermissionName(), role);
+        }
         return permission;
     }
-    public static List<Permission> fromDto(List<PermissionDto> dto){
-        return dto.stream().map(PermissionDtoMapper::fromDto).collect(Collectors.toList());
+
+    public static List<Permission> fromDto(List<PermissionDto> dto) {
+        return dto == null ? null : dto.stream().map(PermissionDtoMapper::fromDto).collect(Collectors.toList());
     }
 
-    public static PermissionDto toDto(Permission permission){
-        return new PermissionDto(permission.getId(), permission.getPermissionName(), permission.getDescription(), permission.getRole().getId());
+    public static PermissionDto toDto(Permission permission) {
+        return permission == null ? null : new PermissionDto(permission.getId(), permission.getPermissionName(), permission.getRole().getId());
     }
-    public static List<PermissionDto> toDto(List<Permission> permissions){
-        return permissions.stream().map(PermissionDtoMapper::toDto).collect(Collectors.toList());
+
+    public static List<PermissionDto> toDto(List<Permission> permissions) {
+        return permissions == null ? null : permissions.stream().map(PermissionDtoMapper::toDto).collect(Collectors.toList());
     }
 }
