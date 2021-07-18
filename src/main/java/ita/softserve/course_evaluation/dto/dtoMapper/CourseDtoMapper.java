9efb07c7932615035d1bcfb7228901f85a76657c
james <jamessss.if@gmail.com>
2021-07-18 17:42:15 +0300

    Completed CourseDtoMapper and CourseDto. Added dependency - ModelMapper

---
+++ src/main/java/ita/softserve/course_evaluation/dto/dtoMapper/CourseDtoMapper.java
@@ -0,0 +1,22 @@
+package ita.softserve.course_evaluation.dto.dtoMapper;
+
+import ita.softserve.course_evaluation.dto.CourseDto;
+import ita.softserve.course_evaluation.entity.Course;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class CourseDtoMapper {
+
+    private ModelMapper mapper;
+
+    public Course toEntity(CourseDto courseDto) {
+        return Objects.isNull(courseDto) ? null : mapper.map(courseDto, Course.class);
+    }
+
+    public CourseDto toDto(Course course) {
+        return Objects.isNull(course) ? null : mapper.map(course, CourseDto.class);
+    }
+}
