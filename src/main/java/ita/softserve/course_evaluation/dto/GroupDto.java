9647a6dc2d9b3963e9770e2459f719d4a0f8eae6
Volodymyr Maliarchuk <vdprogr@gmail.com>
2021-07-18 14:21:23 +0300

    GroupDto with mrthods

--- src/main/java/ita/softserve/course_evaluation/dto/GroupDto.java
+++ src/main/java/ita/softserve/course_evaluation/dto/GroupDto.java
@@ -1,5 +1,7 @@
 package ita.softserve.course_evaluation.dto;
 
+import java.util.Objects;
+
 public class GroupDto {
 
     private Long id;
@@ -28,4 +30,26 @@ public class GroupDto {
     public void setGroupName(String groupName) {
         this.groupName = groupName;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupDto groupDto = (GroupDto) o;
+        return Objects.equals(id, groupDto.id) && Objects.equals(groupName, groupDto.groupName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, groupName);
+    }
+
+    @Override
+    public String toString() {
+        return "GroupDto{" +
+                "id=" + id +
+                ", groupName='" + groupName + '\'' +
+                '}';
+    }
 }
+
