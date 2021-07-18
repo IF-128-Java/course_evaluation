cb50c48b34a9afa0b2b6b29b71d2f7ffb4b72a5b
james <jamessss.if@gmail.com>
2021-07-18 13:47:16 +0300

    Completed CourseController, added CourseDtoMapper and CourseNotFoundException

--- src/main/java/ita/softserve/course_evaluation/controller/CourseController.java
+++ src/main/java/ita/softserve/course_evaluation/controller/CourseController.java
@@ -2,26 +2,24 @@ package ita.softserve.course_evaluation.controller;
 
 import ita.softserve.course_evaluation.dto.CourseDto;
 import ita.softserve.course_evaluation.entity.Course;
+import ita.softserve.course_evaluation.exception.CourseNotFoundException;
 import ita.softserve.course_evaluation.repository.CourseRepository;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.List;
 
 @RestController
-@RequestMapping("api/course")
+@RequestMapping("api/courses/")
 //@RequiredArgsConstructor
 public class CourseController {
     private final CourseRepository courseRepository;
+    private Object CourseNotFoundException;
 
     public CourseController(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
     }
 
-    @RequestMapping(method = {RequestMethod.POST})
+    @PostMapping
     public void postCourse(@RequestBody CourseDto courseDto) {
         Course course = new Course();
         course.setCourseName(courseDto.getCourseName());
@@ -31,5 +29,29 @@ public class CourseController {
         courseRepository.save(course);
     }
 
-    @GetMapping("/api/course/{id}")
+    @PutMapping("/{id}")
+    public void putCourse(@PathVariable int id, @RequestBody CourseDto courseDto) {
+        Course course = new Course();
+        course.setId(id);
+        course.setCourseName(courseDto.getCourseName());
+        course.setDescription(courseDto.getDescription());
+        course.setStartDate(courseDto.getStartDate());
+        course.setEndDate(courseDto.getEndDate());
+        courseRepository.save(course);
+    }
+
+    @GetMapping("/{id}")
+    public Course getCourseById(@PathVariable int id) {
+        return courseRepository.findById(id).orElseThrow(CourseNotFoundException::new);
+    }
+
+    @GetMapping
+    public List<Course> getCourses() {
+        return (List<Course>) courseRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCourse(@PathVariable int id) {
+        courseRepository.deleteById(id);
+    }
 }
---
+++ src/main/java/ita/softserve/course_evaluation/exception/CourseNotFoundException.java
@@ -0,0 +1,11 @@
+package ita.softserve.course_evaluation.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CourseNotFoundException extends RuntimeException {
+    public CourseNotFoundException() {
+        super();
+    }
+}
