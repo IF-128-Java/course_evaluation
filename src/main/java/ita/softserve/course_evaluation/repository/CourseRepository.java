59b0f77016fb91ff648cca07e75aa3a464ef394b
james <jamessss.if@gmail.com>
2021-07-18 23:52:36 +0300

    Updated CourseRepository, added CourseDtoMapper, CourseService and CourseServiceImpl

--- src/main/java/ita/softserve/course_evaluation/repository/CourseRepository.java
+++ src/main/java/ita/softserve/course_evaluation/repository/CourseRepository.java
@@ -4,7 +4,10 @@ import ita.softserve.course_evaluation.entity.Course;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CourseRepository extends CrudRepository<Course, Integer> {
 
+    Optional<Course> findByCourseName(String courseName);
 }
