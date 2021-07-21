c4007ca514d235699b465902c80407ae752d3eee
Volodymyr Maliarchuk <vdprogr@gmail.com>
2021-07-21 21:05:06 +0300

    added refference OneToMany on User table and ManyToMany on Course table

--- src/main/java/ita/softserve/course_evaluation/entity/Group.java
+++ src/main/java/ita/softserve/course_evaluation/entity/Group.java
@@ -5,8 +5,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
 
 @Entity
 @Table(name = "groups")
@@ -19,6 +28,16 @@ public class Group {
 
     @Column(name = "group_name")
     private String groupName;
+
+    @OneToMany(mappedBy = "group", orphanRemoval = true)
+    private List<User> users = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "course_group",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private Set<Course> courses = new HashSet<>();
 
     public Group() {
     }
@@ -41,6 +60,14 @@ public class Group {
 
     public void setGroupName(String groupName) {
         this.groupName = groupName;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
     }
 
     @Override
