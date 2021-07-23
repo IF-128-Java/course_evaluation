4213e5f7699af6616f0f8c780dafd6fa77ff8ef2
James-Koval <jamessss.if@gmail.com>
2021-07-23 11:05:43 +0300

    Update and fixing

--- src/main/java/ita/softserve/course_evaluation/entity/Course.java
+++ src/main/java/ita/softserve/course_evaluation/entity/Course.java
@@ -2,7 +2,18 @@ package ita.softserve.course_evaluation.entity;
 
 import com.sun.istack.NotNull;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import java.io.Serializable;
 import java.util.Date;
 
