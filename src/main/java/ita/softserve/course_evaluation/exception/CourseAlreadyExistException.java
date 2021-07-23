0657f900207d00c8cef88e51018312d176a9f4f1
james <jamessss.if@gmail.com>
2021-07-23 08:28:48 +0300

    Edited and fixed

--- src/main/java/ita/softserve/course_evaluation/exception/CourseAlreadyExistException.java
+++ src/main/java/ita/softserve/course_evaluation/exception/CourseAlreadyExistException.java
@@ -4,13 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class CourseNotFoundException extends RuntimeException {
+public class CourseAlreadyExistException extends RuntimeException {
 
-    public CourseNotFoundException() {
+    public CourseAlreadyExistException() {
         super();
     }
 
-    public CourseNotFoundException(String message) {
+    public CourseAlreadyExistException(String message) {
         super(message);
     }
 
