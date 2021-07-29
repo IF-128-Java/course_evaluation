8109adc4bc813b4c1521c4e1b0b4becacb33bb5d
Arsen-Kushnir <arsikkushnir@gmail.com>
2021-07-29 22:11:13 +0300

    Fixed!

--- src/main/java/ita/softserve/course_evaluation/entity/Course.java
+++ src/main/java/ita/softserve/course_evaluation/entity/Course.java
@@ -5,7 +5,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.io.Serializable;
 import java.util.Date;
 
