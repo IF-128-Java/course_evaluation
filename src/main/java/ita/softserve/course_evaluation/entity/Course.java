764701a1eb68f81d980c92a292ba592c3041fbdc
Arsen-Kushnir <arsikkushnir@gmail.com>
2021-07-29 22:02:43 +0300

    Fixed Course!

--- src/main/java/ita/softserve/course_evaluation/entity/Course.java
+++ src/main/java/ita/softserve/course_evaluation/entity/Course.java
@@ -2,26 +2,14 @@ package ita.softserve.course_evaluation.entity;
 
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -30,30 +18,24 @@ public class Course implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Getter @Setter
     private long id;
 
     @NotNull
     @Column(name = "course_Name", nullable = false)
-    @Getter @Setter
     private String courseName;
 
     @Column(name = "description", nullable = false)
-    @Getter @Setter
     private String description;
 
     @Column(name = "start_Date", nullable = false)
     @Temporal(TemporalType.DATE)
-    @Getter @Setter
     private Date startDate;
 
     @Column(name = "end_Date", nullable = false)
     @Temporal(TemporalType.DATE)
-    @Getter @Setter
     private Date endDate;
 
     @ManyToOne(cascade= CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "teacher_id", nullable = false)
-    @Getter @Setter
     private User user;
 }
