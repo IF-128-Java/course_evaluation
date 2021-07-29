85f6c24c827c4529f4e149cfadeff14f18c74d89
Volodymyr Maliarchuk <vdprogr@gmail.com>
2021-07-29 13:31:19 +0300

    remove import to @Qualifier

--- src/main/java/ita/softserve/course_evaluation/security/jwt/JwtTokenProvider.java
+++ src/main/java/ita/softserve/course_evaluation/security/jwt/JwtTokenProvider.java
@@ -6,7 +6,6 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import ita.softserve.course_evaluation.exception.JwtAuthenticationException;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
