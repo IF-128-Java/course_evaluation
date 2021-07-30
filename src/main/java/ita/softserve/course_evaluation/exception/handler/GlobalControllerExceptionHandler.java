2cfa1965d40f08afe1e7df9c783fae4f3a5d8fd7
FedenkoM <fedenkomisha@gmail.com>
2021-07-31 00:04:06 +0300

    add exception handler

---
+++ src/main/java/ita/softserve/course_evaluation/exception/handler/GlobalControllerExceptionHandler.java
@@ -0,0 +1,77 @@
+package ita.softserve.course_evaluation.exception.handler;
+
+import ita.softserve.course_evaluation.exception.CourseAlreadyExistException;
+import ita.softserve.course_evaluation.exception.CourseNotFoundException;
+import ita.softserve.course_evaluation.exception.JwtAuthenticationException;
+import ita.softserve.course_evaluation.exception.dto.GenericExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ExceptionHandler({CourseNotFoundException.class})
+    public ResponseEntity<GenericExceptionResponse> handleCourseNotFoundException(CourseNotFoundException exception) {
+
+        GenericExceptionResponse dto = GenericExceptionResponse.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.NOT_FOUND.value())
+                .error(exception.getClass().getSimpleName())
+                .build();
+
+        return new ResponseEntity<>(dto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({CourseAlreadyExistException.class})
+    public ResponseEntity<GenericExceptionResponse> handleCourseAlreadyExistException(CourseAlreadyExistException exception) {
+
+        GenericExceptionResponse dto = GenericExceptionResponse.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error(exception.getClass().getSimpleName())
+                .build();
+
+        return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({JwtAuthenticationException.class})
+    public ResponseEntity<GenericExceptionResponse> handleJwtAuthenticationException(JwtAuthenticationException exception) {
+
+        GenericExceptionResponse dto = GenericExceptionResponse.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.FORBIDDEN.value())
+                .error(exception.getClass().getSimpleName())
+                .build();
+
+        return new ResponseEntity<>(dto, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler({EntityNotFoundException.class})
+    public ResponseEntity<GenericExceptionResponse> handleEntityNotFoundException(EntityNotFoundException exception) {
+
+        GenericExceptionResponse dto = GenericExceptionResponse.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.NOT_FOUND.value())
+                .error(exception.getClass().getSimpleName())
+                .build();
+
+        return new ResponseEntity<>(dto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<GenericExceptionResponse> handleAllException(Exception exception) {
+
+        GenericExceptionResponse dto = GenericExceptionResponse.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .error(exception.getClass().getSimpleName())
+                .build();
+
+        return new ResponseEntity<>(dto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
