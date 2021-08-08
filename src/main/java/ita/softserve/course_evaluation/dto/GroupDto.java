27fef9fc7b2c0ab97d8a5f3be67937cdc184fed0
Volodymyr Maliarchuk <vdprogr@gmail.com>
2021-08-08 16:03:42 +0300

    changed GroupDtoMapper

--- src/main/java/ita/softserve/course_evaluation/dto/GroupDto.java
+++ src/main/java/ita/softserve/course_evaluation/dto/GroupDto.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,4 +14,7 @@ public class GroupDto {
 
     private Long id;
     private String groupName;
+    private List<UserDto> students = new ArrayList<>();
+    private List<CourseDto> courses = new ArrayList<>();
+
 }
--- src/main/java/ita/softserve/course_evaluation/dto/GroupDtoMapper.java
+++ src/main/java/ita/softserve/course_evaluation/dto/GroupDtoMapper.java
@@ -1,5 +1,6 @@
 package ita.softserve.course_evaluation.dto;
 
+import ita.softserve.course_evaluation.dto.dtoMapper.CourseDtoMapper;
 import ita.softserve.course_evaluation.entity.Group;
 
 import java.util.List;
@@ -11,13 +12,11 @@ public class GroupDtoMapper {
     }
 
     public static GroupDto entityToDto(Group group) {
-
-        if (group == null) return null;
-
         GroupDto groupdto = new GroupDto();
         groupdto.setId(group.getId());
         groupdto.setGroupName(group.getGroupName());
-
+        groupdto.setStudents(UserDtoMapper.toDto(group.getUsers()));
+        groupdto.setCourses(CourseDtoMapper.toDto(group.getCourses()));
         return groupdto;
     }
 
@@ -27,13 +26,11 @@ public class GroupDtoMapper {
     }
 
     public static Group dtoToEntity(GroupDto groupdto) {
-
-        if (groupdto == null) return null;
-
         Group group = new Group();
         group.setId(groupdto.getId());
         group.setGroupName(groupdto.getGroupName());
-
+        group.setUsers(UserDtoMapper.fromDto(groupdto.getStudents()));
+        group.setCourses(CourseDtoMapper.toEntity(groupdto.getCourses()));
         return group;
     }
 
