1bb3da43844a9c128df1b42696428a4cc9629e81
james <jamessss.if@gmail.com>
2021-08-13 14:46:08 +0300

    Courses list refactoring

--- src/main/java/ita/softserve/course_evaluation/repository/CourseRepository.java
+++ src/main/java/ita/softserve/course_evaluation/repository/CourseRepository.java
@@ -12,5 +12,11 @@ import java.util.Optional;
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
+    @Query(value = "SELECT c.id, course_name, description, start_date, end_date, teacher_id, first_name, last_name FROM course c LEFT JOIN users u ON c.teacher_id = u.id", nativeQuery = true)
+    List<Course> findAllCourses();
+
+    @Query(value = "SELECT c.id, course_name, description, start_date, end_date, teacher_id, first_name, last_name, ur.role_id FROM course c LEFT JOIN users u ON c.teacher_id = u.id LEFT JOIN user_roles ur on u.id = ur.user_id WHERE role_id = 1", nativeQuery = true)
+    Course findCourseByIdWithTeachers();
+
     List<Course> findByCourseName(String courseName);
 }
