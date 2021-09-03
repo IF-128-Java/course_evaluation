fab19d0b7bcb86f5636ea633cc10ed738809962d
FedenkoM <fedenkomisha@gmail.com>
2021-09-03 14:08:32 +0300

    add tests for RegistrationServiceImpl

--- src/test/java/ita/softserve/course_evaluation/service/UserServiceImplTests.java
+++ src/test/java/ita/softserve/course_evaluation/service/UserServiceImplTests.java
@@ -26,7 +26,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.lenient;
