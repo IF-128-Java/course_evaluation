0c218e400227131fbfd72b8df332f7a128e00850
Arsen-Kushnir <arsikkushnir@gmail.com>
2021-09-13 22:51:55 +0300

    Teacher chat!

--- src/main/java/ita/softserve/course_evaluation/repository/ChatRoomRepository.java
+++ src/main/java/ita/softserve/course_evaluation/repository/ChatRoomRepository.java
@@ -2,8 +2,12 @@ package ita.softserve.course_evaluation.repository;
 
 import ita.softserve.course_evaluation.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    @Query(value = "SELECT id FROM chat_rooms WHERE chat_type = 'TEACHER'", nativeQuery = true)
+    Long getTeacherChatRoomId();
 }
